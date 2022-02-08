cc2d8871d69b926172729799dbfd4faafd1d9c2a
ankit <ankitkohli181@gmail.com>
2022-02-08 12:10:15 +0530

    creating test folder

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,5 @@
 FROM node:16
 RUN apt-get update
-RUN apt install -y unoconv
-RUN apt install -y libreoffice
 COPY . /app
 WORKDIR /app
 RUN npm install
--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express=require('express');
 const app=express();
+const fs=require('fs');
+const path=require('path');
 const bodyParser=require("body-parser");
 app.use(bodyParser.urlencoded({extended:true}));
 
@@ -7,6 +9,12 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/",function(req,res){
     res.send("App running successfully !!")
+})
+app.get("/createdir",function(req,res){
+    if (!fs.existsSync(path.join(__dirname,'/testfolder'))) {
+        fs.mkdirSync(path.join(__dirname,'/testfolder'));
+        res.send("directory created successfully !!")
+    }
 })
 
 app.listen(3000 || process.env.PORT ,function(){
